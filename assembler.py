8935ea7007c45f74d4f31409b83300f93faed094
Ekaksher Khajuria <ekaksher21@gmail.com>
2020-10-30 22:30:51 +0530

    Added CLI

--- assembler.py
+++ assembler.py
@@ -1,11 +1,13 @@
 from src.lexer.lexer import Lexer
 from src.parser.parser import Parser
-
-# argparse definitions
-
+from io import StringIO
+import argparse
 def get_parser():
     lexer = Lexer()
     return Parser(lexer=lexer)
-
-# get_parser() -> parser
-# parser.assemble(input_stream, output_stream)
+ARGS_PARSER = argparse.ArgumentParser()
+ARGS_PARSER.add_argument("input", help = "source file")
+ARGS_PARSER.add_argument("-o","--output",default="a.txt",help="output file name")
+ARGS = ARGS_PARSER.parse_args()
+Parser = get_parser()
+Parser.assemble(open(ARGS.input,'r'),open(ARGS.output,'w+'))
